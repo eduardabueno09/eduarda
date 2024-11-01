ef0fbdb7ea441c0780bd12099a60d6e49d765cf9
eduardabueno09 <ev108957@gmail.com>
2024-10-31 22:44:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'Para que o Python é usado?',
+    'Desenvolvimento web, análise de dados e IA'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'história',
+    'Causa da queda do Império Romano?',
+    'Pressões externas e crises internas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'portugues',
+    'O que é um substantivo?',
+    'Nome de pessoas, lugares ou coisas'
+)
+
+criaCartao(
+    'portugues',
+    'O que é um verbo?',
+    'Palavra que indica ação ou estado'
+)
